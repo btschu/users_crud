21c56bd335cc6e861652573c73c6033e5f757fb9
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2022-03-09 19:45:16 -0700

    cleaned up code and templates

--- server.py
+++ server.py
@@ -3,13 +3,20 @@ from flask import Flask, redirect, request, render_template
 
 app = Flask(__name__)
 
+# View ALL USERS
+
 @app.route('/')
 def index():
     return redirect('/users')
 
 @app.route('/users')
 def users():
-    return render_template("read_all.html", users = User.get_all())
+    context = {
+        'users' : User.get_all()
+    }
+    return render_template("read_all.html", **context)
+
+# Create NEW USER
 
 @app.route('/users/new')
 def new():
@@ -20,24 +27,36 @@ def create():
     User.save(request.form)
     return redirect('/users')
 
+# EDIT USER
+
 @app.route('/user/edit/<int:id>')
 def edit(id):
     data ={
         "id":id
     }
-    return render_template("edit.html", user = User.get_one(data))
+    context = {
+        'user' : User.get_one(data)
+    }
+    return render_template("edit.html", **context)
+
+@app.route('/user/update',methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+# SHOW one user
 
 @app.route('/user/show/<int:id>')
 def show(id):
     data ={
         "id":id
     }
-    return render_template("read_one.html", user = User.get_one(data))
+    context = {
+        'user' : User.get_one(data)
+    }
+    return render_template("read_one.html", **context)
 
-@app.route('/user/update',methods=['POST'])
-def update():
-    User.update(request.form)
-    return redirect('/users')
+# DELETE one user
 
 @app.route('/user/delete/<int:id>')
 def delete(id):
@@ -46,7 +65,6 @@ def delete(id):
     }
     User.delete(data)
     return redirect('/users')
-
 
 if __name__ == '__main__':
     app.run(debug = True)
--- user.py
+++ user.py
@@ -1,5 +1,7 @@
 from mysqlconnection import connectToMySQL
 
+db = "users_schema"
+
 class User:
 
     def __init__( self , data ):
@@ -11,31 +13,40 @@ class User:
         self.updated_at = data['updated_at']
 
     @classmethod
-    def get_one(cls,data):
-        query  = "SELECT * FROM users WHERE id = %(id)s";
-        result = connectToMySQL('users_schema').query_db(query,data)
-        return cls(result[0])
+    def save(cls, data ):
+        query = """
+        INSERT INTO users ( first_name, last_name, email, created_at, updated_at )
+        VALUES ( %(fname)s, %(lname)s, %(email)s, NOW(), NOW() );"""
+        return connectToMySQL(db).query_db( query, data )
 
     @classmethod
     def update(cls,data):
-        query = "UPDATE users SET first_name=%(fname)s,last_name=%(lname)s,email=%(email)s,updated_at=NOW() WHERE id = %(id)s;"
-        return connectToMySQL('users_schema').query_db(query,data)
+        query = """
+        UPDATE users
+        SET first_name = %(fname)s, last_name = %(lname)s, email = %(email)s, updated_at = NOW()
+        WHERE id = %(id)s;"""
+        return connectToMySQL(db).query_db(query,data)
 
     @classmethod
     def delete(cls,data):
-        query  = "DELETE FROM users WHERE id = %(id)s;"
-        return connectToMySQL('users_schema').query_db(query,data)
+        query  = """
+        DELETE FROM users
+        WHERE id = %(id)s;"""
+        return connectToMySQL(db).query_db(query,data)
+
+    @classmethod
+    def get_one(cls,data):
+        query  = """
+        SELECT * FROM users
+        WHERE id = %(id)s;"""
+        result = connectToMySQL(db).query_db(query,data)
+        return cls(result[0])
 
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        results = connectToMySQL('users_schema').query_db(query)
+        results = connectToMySQL(db).query_db(query)
         users = []
         for user in results:
             users.append( cls(user) )
         return users
-
-    @classmethod
-    def save(cls, data ):
-        query = "INSERT INTO users ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(email)s , NOW() , NOW() );"
-        return connectToMySQL('users_schema').query_db( query, data )
